d297871fa2e7143788b4e44c261ee68f483374bb
jxmeslukin <jimmy.lukin@gmail.com>
2022-02-23 14:00:30 +1100

    movement

--- js/main.js
+++ js/main.js
@@ -60,3 +60,61 @@ tl.from(".splash", 1.8, {
     opacity: 0
 });
 
+
+
+console.clear();
+
+const can = document.querySelector(".splash");
+
+const randomX = random(4, 8);
+const randomY = random(3, 6);
+const randomDelay = random(0, 1);
+const randomTime = random(3, 5);
+const randomTime2 = random(5, 10);
+const randomAngle = random(0, 4);
+
+TweenLite.set(can, {
+  x: randomX(-1),
+  y: randomX(1),
+  rotation: randomAngle(-1)
+});
+
+moveX(can, 1);
+moveY(can, -1);
+rotate(can, 1);
+
+function rotate(target, direction) {
+  
+  TweenLite.to(target, randomTime2(), {
+    rotation: randomAngle(direction),
+    // delay: randomDelay(),
+    ease: Sine.easeInOut,
+    onComplete: rotate,
+    onCompleteParams: [target, direction * -1]
+  });
+}
+
+function moveX(target, direction) {
+  
+  TweenLite.to(target, randomTime(), {
+    x: randomX(direction),
+    ease: Sine.easeInOut,
+    onComplete: moveX,
+    onCompleteParams: [target, direction * -1]
+  });
+}
+
+function moveY(target, direction) {
+  
+  TweenLite.to(target, randomTime(), {
+    y: randomY(direction),
+    ease: Sine.easeInOut,
+    onComplete: moveY,
+    onCompleteParams: [target, direction * -1]
+  });
+}
+
+function random(min, max) {
+  const delta = max - min;
+  return (direction = 1) => (min + delta * Math.random()) * direction;
+}
