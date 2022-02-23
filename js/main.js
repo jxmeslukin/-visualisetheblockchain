461165ab225eaa8b14e6b3aca6a4f3531d99c829
jxmeslukin <jimmy.lukin@gmail.com>
2022-02-23 11:06:53 +1100

    fixes

---
+++ js/main.js
@@ -0,0 +1,62 @@
+/*
+
+Author: James Lukin
+'visualisetheblockchain'
+
+*/
+
+gsap.registerPlugin(ScrollTrigger);
+// REVEAL //
+gsap.utils.toArray(".revealUp").forEach(function (elem) {
+  ScrollTrigger.create({
+    trigger: elem,
+    start: "top 80%",
+    end: "bottom 20%",
+    markers: false,
+    onEnter: function () {
+      gsap.fromTo(
+        elem,
+        { y: 100, autoAlpha: 0 },
+        {
+          duration: 1.25,
+          y: 0,
+          autoAlpha: 1,
+          ease: "back",
+          overwrite: "auto"
+        }
+      );
+    },
+    onLeave: function () {
+      gsap.fromTo(elem, { autoAlpha: 1 }, { autoAlpha: 0, overwrite: "auto" });
+    },
+    onEnterBack: function () {
+      gsap.fromTo(
+        elem,
+        { y: -100, autoAlpha: 0 },
+        {
+          duration: 1.25,
+          y: 0,
+          autoAlpha: 1,
+          ease: "back",
+          overwrite: "auto"
+        }
+      );
+    },
+    onLeaveBack: function () {
+      gsap.fromTo(elem, { autoAlpha: 1 }, { autoAlpha: 0, overwrite: "auto" });
+    }
+  });
+});
+
+var tl = gsap.timeline();
+tl.from(".splash", 1.8, {
+    y: 100,
+    ease: 'power4.out',
+    delay: 0,
+    skewY: 0,
+    stagger: {
+        amount: 0.2
+    },
+    opacity: 1
+});
+
